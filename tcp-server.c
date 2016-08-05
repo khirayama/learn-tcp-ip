3fd74fdd49dd3ca47611d24e805999a7e52e7d74
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-08-06 08:15:57 +0900

    Update tcp-server

--- tcp-server.c
+++ tcp-server.c
@@ -9,31 +9,45 @@ int main() {
   struct sockaddr_in client;
   int len;
   int sock;
+  int n;
 
-  /* ソケットの作成 */
   sock0 = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock0 < 0) {
+    perror("socket");
+    return 1;
+  }
 
-  /* ソケットの設定 */
   addr.sin_family = AF_INET;
   addr.sin_port = htons(12345);
   addr.sin_addr.s_addr = INADDR_ANY;
 
-  bind(sock0, (struct sockaddr *)&addr, sizeof(addr));
+  if (bind(sock0, (struct sockaddr *)&addr, sizeof(addr)) != 0) {
+    perror("bind");
+    return 1;
+  }
 
-  /* TCPクライアントからの接続要求を待てる状態にする */
-  listen(sock0, 5);
+  if (listen(sock0, 5) != 0) {
+    perror("listen");
+    return 1;
+  }
 
-  /* TCPクライアントからの接続要求を受け付ける */
-  len = sizeof(client);
-  sock = accept(sock0, (struct sockaddr *)&client, &len);
+  while (1) {
+    len = sizeof(client);
+    sock = accept(sock0, (struct sockaddr *)&client, &len);
+    if (sock < 0) {
+      perror("accept");
+      break;
+    }
 
-  /* 5文字送信 */
-  write(sock, "HELLO", 5);
+    n = write(sock, "HELLO", 5);
+    if (n < 1) {
+      perror("write");
+      break;
+    }
 
-  /* TCPセッションの終了 */
-  close(sock);
+    close(sock);
+  }
 
-  /* listen するsocketの終了 */
   close(sock0);
 
   return 0;
